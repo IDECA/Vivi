2fae8522286232f8ea911e59c79c1087c74f8617
David Bello <dhbello@hotmail.com>
2013-07-15 10:38:59 -0500

    Version 0.61

--- index.js
+++ index.js
@@ -261,6 +261,7 @@ function mapLoadHandler(map) {
     } else {
         map.centerAndZoom(new esri.geometry.Point(-74.075833, 4.598056, map.spatialReference), 5);
         currentPoint = new esri.geometry.Point(-74.075833, 4.598056, map.spatialReference);
+
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(zoomToLocation, null);
         }
